172c2587df0a5b5d161244f5f16a6c4520ab60c5
Ubuntu <ubuntu@crams---jenkins.novalocal>
2016-01-18 11:10:44 +1100

    made script

---
+++ benchmark_cpumem.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+now=`date +'%Y%m%d_%H%M'`
+filename="benchmark_cpumem_${now}_results"
+
+print "Running benchmarks and writing results to ${filename}"
+
+sysbench --test=cpu --cpu-max-prime=20000 run >> ${filename}
+sysbench --test=cpu --cpu-max-prime=20000 --num-threads=2 run >> ${filename}
+sysbench --test=threads --thread-locks=1 --max-time=20s run >> ${filename}
+sysbench --test=mutex --num-threads=64 run >> ${filename}
+sysbench --test=memory --num-threads=2 run >> ${filename}
